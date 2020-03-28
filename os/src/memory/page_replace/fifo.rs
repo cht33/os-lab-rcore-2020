6f535fbc418613184f3818547bcc471439b2dce4
陈海天 <cht16@mails.tsinghua.edu.cn>
2020-03-28 20:18:41 +0800

    finish lab3.

--- os/src/memory/page_replace/fifo.rs
+++ os/src/memory/page_replace/fifo.rs
@@ -7,6 +7,7 @@ use {
 #[derive(Default)]
 pub struct FifoPageReplace {
     frames: VecDeque<(usize, Arc<Mutex<PageTableImpl>>)>,
+    p: usize, // 时钟替换算法的当前位置
 }
 
 impl PageReplace for FifoPageReplace {
@@ -17,7 +18,18 @@ impl PageReplace for FifoPageReplace {
 
     fn choose_victim(&mut self) -> Option<(usize, Arc<Mutex<PageTableImpl>>)> {
         // 选择一个已经分配的物理页帧
-        self.frames.pop_front()
+        let len = self.frames.len();
+        loop {
+            self.p = self.p % len;
+            let (vaddr, pt) = self.frames.get(self.p).unwrap();
+            if let Some(entry) = pt.lock().get_entry(*vaddr) {
+                if entry.accessed() {
+                    entry.clear_accessed();
+                    self.p += 1;
+                } else { break; }
+            } else { panic!("invalid page when choose_victim") }
+        }
+        self.frames.remove(self.p)
     }
 
     fn tick(&self) {}
